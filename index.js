6329ea0578177b947dc1e69bb223b71b69d2dbb5
MaxwelSantana <maxwelsant96@icloud.com>
2020-06-08 22:34:23 -0300

    added call to brasil.io api

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
 
+const axios = require('axios');
+
 app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+    //api dados recentes por estado Brasil.IO
+    axios.get('https://brasil.io/api/dataset/covid19/caso/data?is_last=True&place_type=state')
+        .then(response => {
+            console.log(response)
+            res.json(response.data);
+        })
+        .catch(error => {
+            res.send(error);
+        });
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
